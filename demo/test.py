184a298b3bd88068ef727016a76e055238266ca3
parallels <andreea.sonda31@gmail.com>
2016-05-18 18:17:57 +0300

    Test my first test new war func

---
+++ demo/test.py
@@ -0,0 +1,24 @@
+#heiii
+'''
+
+ some code
+'''
+
+#import six.moves -rezolva compatibilitatea intr python2 si python3
+
+# my_string = 'test'
+# my_string = "test2"
+# c=int(raw_input('Number: '))
+# print('Your number %s' % c)
+##my_dict={'test':10,
+		# 'abc' : [1,2,'test','test3'],
+		# 'c' : dict(a=10),
+		# 'cd': {'a':10}
+ 		# }
+# print my_dict
+# print my_dict['abc']
+# my_dict['c'] ='a'
+#print dir(my_dict)
+a=range(10)
+print a[2:-1:2]
+print 
